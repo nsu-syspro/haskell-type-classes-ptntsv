78a3ad37edc50e5d586771b99a977339fd72b300
ptntsv <hitogavax@ya.ru>
2026-03-07 21:12:03 +0700

    Solution

--- src/Task2.hs
+++ src/Task2.hs
@@ -1,11 +1,12 @@
-{-# OPTIONS_GHC -Wall #-}
 -- The above pragma enables all warnings
-
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module Task2 where
 
-import Task1 (Parse, Parse(..))
+import Data.List (nub)
+import Task1 (Parse (..))
 
 -- * Expression data type
 
@@ -13,15 +14,29 @@ import Task1 (Parse, Parse(..))
 -- - Literals of type 'a'
 -- - Variables with arbitrary 'String' names
 -- - Binary operations of type 'op'
-data Expr a op =
-    Lit a
+data Expr a op
+  = Lit a
   | Var String
   | BinOp op (Expr a op) (Expr a op)
-  deriving Show
+  deriving (Show)
+
+getVars :: Expr a op -> [String]
+getVars = nub . getVars'
+  where
+    getVars' (Var name) = [name]
+    getVars' (BinOp _ l r) = getVars l ++ getVars r
+    getVars' _ = []
 
 -- | Integer binary operations
 data IntOp = Add | Mul | Sub
-  deriving Show
+  deriving (Show)
+
+instance Parse IntOp where
+  parse :: String -> Maybe IntOp
+  parse "*" = Just Mul
+  parse "+" = Just Add
+  parse "-" = Just Sub
+  parse _ = Nothing
 
 -- * Parsing
 
@@ -40,16 +55,33 @@ data IntOp = Add | Mul | Sub
 -- Nothing
 -- >>> parse "2 3" :: Maybe (Expr Integer IntOp)
 -- Nothing
---
 instance (Parse a, Parse op) => Parse (Expr a op) where
-  parse = error "TODO: define parse (Parse (Expr a op))"
+  parse input = go (words input) []
+    where
+      go :: [String] -> [Expr a op] -> Maybe (Expr a op)
+      go [] [result] = Just result
+      go [] _ = Nothing
+      go (t : ts) stack =
+        case (parse t :: Maybe op) of
+          Just oper -> case stack of
+            (e1 : e2 : es) -> go ts (BinOp oper e2 e1 : es)
+            _ -> Nothing
+          _ -> case (parse t :: Maybe a) of
+            Just n -> go ts (Lit n : stack)
+            _ -> go ts (Var t : stack)
 
 -- * Evaluation
 
 -- | Class of evaluatable types
 class Eval a op where
-  -- | Evaluates given binary operation with provided arguments
+  -- \| Evaluates given binary operation with provided arguments
   evalBinOp :: op -> a -> a -> a
+
+instance Eval Integer IntOp where
+  evalBinOp :: IntOp -> Integer -> Integer -> Integer
+  evalBinOp Add l r = l + r
+  evalBinOp Mul l r = l * r
+  evalBinOp Sub l r = l - r
 
 -- | Evaluates given 'Expr' using given association list of variable values
 --
@@ -63,9 +95,12 @@ class Eval a op where
 -- Just 5
 -- >>> evalExpr [("x", 3)] (BinOp Add (Lit 2) (Var "y")) :: Maybe Integer
 -- Nothing
---
 evalExpr :: (Eval a op) => [(String, a)] -> Expr a op -> Maybe a
-evalExpr = error "TODO: define evalExpr"
+evalExpr table (BinOp oper le re) = case (evalExpr table le, evalExpr table re) of
+  (Just lv, Just rv) -> Just $ evalBinOp oper lv rv
+  _ -> Nothing
+evalExpr _ (Lit n) = Just n
+evalExpr table (Var name) = lookup name table
 
 -- | Parses given integer expression in Reverse Polish Notation and evaluates it
 -- using given association list of variable values
@@ -87,9 +122,8 @@ evalExpr = error "TODO: define evalExpr"
 -- Nothing
 -- >>> evaluateInteger [] "2 3"
 -- Nothing
---
 evaluateInteger :: [(String, Integer)] -> String -> Maybe Integer
-evaluateInteger = error "TODO: define evaluateInteger"
+evaluateInteger = evaluate @_ @IntOp
 
 -- | Parses given expression in Reverse Polish Notation and evaluates it
 -- using given association list of variable values
@@ -99,9 +133,7 @@ evaluateInteger = error "TODO: define evaluateInteger"
 --
 -- The 'forall a op.' part is required to define generic type
 -- of intermediate 'Expr' expression that uses scoped type variables 'a' and 'op'.
---
 evaluate :: forall a op. (Eval a op, Parse a, Parse op) => [(String, a)] -> String -> Maybe a
 evaluate m s = case parse s of
   Just e -> evalExpr m (e :: Expr a op)
   Nothing -> Nothing
-
--- src/Task3.hs
+++ src/Task3.hs
@@ -1,7 +1,29 @@
-{-# OPTIONS_GHC -Wall #-}
 -- The above pragma enables all warnings
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module Task3 where
+
+import Control.Monad (replicateM)
+import Data.Maybe (fromMaybe)
+import Task1 (Parse (parse))
+import Task2
+
+data BoolOp = And | Or | Xor
+  deriving (Show)
+
+instance Parse BoolOp where
+  parse "and" = Just And
+  parse "or" = Just Or
+  parse "xor" = Just Xor
+  parse _ = Nothing
+
+instance Eval Bool BoolOp where
+  evalBinOp :: BoolOp -> Bool -> Bool -> Bool
+  evalBinOp And l r = l && r
+  evalBinOp Or l r = l || r
+  evalBinOp Xor l r = l /= r
 
 -- | Solves SAT problem for given boolean formula written in Reverse Polish Notation
 --
@@ -25,6 +47,11 @@ module Task3 where
 -- Just True
 -- >>> solveSAT "x xor"
 -- Nothing
---
 solveSAT :: String -> Maybe Bool
-solveSAT = error "TODO: define solveSAT"
+solveSAT s = case parse s of
+  Just (e :: Expr Bool BoolOp) -> Just $ any (fromMaybe False . evalBoolExpr) possibleValues
+    where
+      evalBoolExpr m = evaluate @Bool @BoolOp m s
+      names = getVars e
+      possibleValues = [zip names vals | vals <- replicateM (length names) [True, False]]
+  _ -> Nothing
